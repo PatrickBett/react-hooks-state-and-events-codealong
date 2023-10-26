32ffa7d5ba71092afa8a29e39a3af5a3c12ee67a
PatrickBett <patrickbett018@gmail.com>
2023-10-26 14:43:10 +0300

    Done

--- src/components/Toggle.js
+++ src/components/Toggle.js
@@ -7,9 +7,10 @@ function Toggle() {
   function handleClick(){
     setIsOn((isOn) => !isOn)
   }
+  const color = isOn ? "red" : "white";
 
 
-  return <button onClick={handleClick}>{ isOn? "ON":"OFF" }</button>;
+  return <button style={{ background: color }} onClick={handleClick}>{ isOn? "ON":"OFF" }</button>;
 }
 
 export default Toggle;
